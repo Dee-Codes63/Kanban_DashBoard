90c5904e709672d57adf299481e585c97e28da98
Dee6370 <115374333+Dee6370@users.noreply.github.com>
2024-11-28 22:53:59 +0530

    my first commit

---
+++ src/icons/images.js
@@ -0,0 +1,35 @@
+import cancel from "./Cancelled.svg";
+import dashed_circle from "./Backlog.svg";
+import done from "./Done.svg";
+import down from "./down.svg";
+import fade_circle from "./fade_circle.png";
+import grey_exclamation from "./SVG - Urgent Priority colour.svg";
+import high from "./Img - High Priority.svg";
+import in_progress from "./in-progress.svg";
+import low from './Img - Low Priority.svg';
+import medium from "./Img - Medium Priority.svg";
+import urgent from './SVG - Urgent Priority colour.svg';
+import plus from "./add.svg";
+import no_priority from "./No-priority.svg";
+import three_dot from './3 dot menu.svg';
+import display_filter from './Display.svg';
+import void_circle from './To-do.svg';
+
+export {
+    cancel,
+    dashed_circle,
+    done,
+    down,
+    fade_circle,
+    grey_exclamation,
+    high,
+    in_progress,
+    low,
+    medium,
+    urgent,
+    plus,
+    no_priority,
+    three_dot,
+    display_filter,
+    void_circle
+}
